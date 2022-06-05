5136cce7eae1d83c9decd30bab9a82f90ec2c569
Gabe-felps <gabriel.macedo@sptech.school>
2022-06-05 19:41:35 -0300

    tela de dashboard não funcional no modelo da acquatec

--- Modelagem de projeto/BDmagnatech.sql
+++ Modelagem de projeto/BDmagnatech.sql
@@ -2,103 +2,90 @@ create database magnatech;
 
 use magnatech;
 
-
 CREATE TABLE fazenda (
-  idFazenda INT,
-  nome VARCHAR(40),
-  razaoSocial VARCHAR(40) ,
-  cnpj CHAR(14) ,
-  email VARCHAR(45) ,
-  telefoneCel CHAR(11) ,
-  telefoneFixo char (10),
-  rua VARCHAR(40) ,
-  numero VARCHAR(40) ,
-  bairro VARCHAR(40) ,
-  cep CHAR(8) ,
-  cidade VARCHAR(40) ,
-  estado VARCHAR(40) ,
-  PRIMARY KEY (idFazenda)
-  );
-
-
-
+	idFazenda INT,
+	nome VARCHAR(40),
+	razaoSocial VARCHAR(40),
+	cnpj CHAR(14),
+	email VARCHAR(45),
+	telefoneCel CHAR(11),
+	telefoneFixo char (10),
+	rua VARCHAR(40),
+	numero VARCHAR(40),
+	bairro VARCHAR(40),
+	cep CHAR(8),
+	cidade VARCHAR(40),
+	estado VARCHAR(40),
+	PRIMARY KEY (idFazenda)
+);
 
 CREATE TABLE especieCogumelo (
-  idCogumelo INT  ,
-  especie VARCHAR(45) not null,
-  tempIdeal DECIMAL(4,2) ,
-  umidIdeal DECIMAL(3,1) ,
-  PRIMARY KEY (idCogumelo))
-  ;
-
-
-
+	idCogumelo INT,
+	especie VARCHAR(45) not null,
+	tempIdeal DECIMAL(4,2),
+	umidIdeal DECIMAL(3,1),
+	PRIMARY KEY (idCogumelo)
+);
 
 CREATE TABLE nicho (
-  fkFazenda INT,
-  idNicho INT,
-  coluna VARCHAR(5),
-  prateleira VARCHAR(5),
-  fkCogumelo INT,
-  PRIMARY KEY (idNicho, fkFazenda),
-    FOREIGN KEY (fkFazenda)
-    REFERENCES fazenda (idFazenda),
-    FOREIGN KEY (fkCogumelo)
-    REFERENCES especieCogumelo (idCogumelo)
-    );
-
+	fkFazenda INT,
+	idNicho INT,
+	coluna VARCHAR(5),
+	prateleira VARCHAR(5),
+	fkCogumelo INT,
+	PRIMARY KEY (idNicho, fkFazenda),
+	FOREIGN KEY (fkFazenda) 
+		REFERENCES fazenda (idFazenda),
+	FOREIGN KEY (fkCogumelo) 
+		REFERENCES especieCogumelo (idCogumelo)
+);
 
 CREATE TABLE Funcionario(
-fkFazenda INT,
-idFuncionario INT AUTO_INCREMENT,
-PRIMARY KEY (idFuncionario, fkFazenda),
-FOREIGN KEY (fkFazenda) REFERENCES fazenda(idFazenda),
-senha VARCHAR(20),
-nome VARCHAR(60),
-celular CHAR(11),
-email VARCHAR(60)
+	fkFazenda INT,
+	idFuncionario INT AUTO_INCREMENT,
+	PRIMARY KEY (idFuncionario, fkFazenda),
+	FOREIGN KEY (fkFazenda) 
+		REFERENCES fazenda(idFazenda),
+	senha VARCHAR(20),
+	nome VARCHAR(60),
+	celular CHAR(11),
+	email VARCHAR(60)
 )AUTO_INCREMENT = 25220;
 
-
-
-
 CREATE TABLE arduino (
-  idSensor INT PRIMARY KEY ,
-  fkNicho INT,
-  fkFazenda INT,
-  sensor varchar(3),
-    FOREIGN KEY (fkNicho , fkFazenda)
-    REFERENCES nicho (idNicho , fkFazenda)
-    );
+	idSensor INT PRIMARY KEY,
+	fkNicho INT,
+	fkFazenda INT,
+	sensor varchar(3),
+	FOREIGN KEY (fkNicho , fkFazenda) 
+		REFERENCES nicho (idNicho , fkFazenda)
+);
 
-
-    CREATE TABLE Captura(
-    idCaptura INT AUTO_INCREMENT,
-   fkSensor INT,
-   FOREIGN KEY (fkSensor) REFERENCES arduino(idSensor),
-   PRIMARY KEY (idCaptura, fkSensor ),
-   umidade DECIMAL(4,2),
+CREATE TABLE Captura(
+	idCaptura INT AUTO_INCREMENT,
+	fkSensor INT,
+	FOREIGN KEY (fkSensor) 
+		REFERENCES arduino(idSensor),
+	PRIMARY KEY (idCaptura, fkSensor),
+	umidade DECIMAL(4,2),
 	temperatura DECIMAL(4,2),
-    momento DATETIME
-    );
-    
-    
-
+	momento DATETIME
+);
 
 CREATE TABLE agendamento (
-  idagendamento INT AUTO_INCREMENT,
-  fkFazenda INT,
-  nomeRepresentante VARCHAR(45) ,
-  telefone CHAR(11) ,
-  email VARCHAR(40) ,
-  agendamentoData DATE ,
-  nomeEmpresa VARCHAR(40) ,
-  cnpj CHAR(14) ,
-  rua VARCHAR(40) ,
-  bairro VARCHAR(40) ,
-  complemento VARCHAR(40) ,
-  PRIMARY KEY (idagendamento)
-  )AUTO_INCREMENT= 10000;
+	idagendamento INT AUTO_INCREMENT,
+	fkFazenda INT,
+	nomeRepresentante VARCHAR(45),
+	telefone CHAR(11),
+	email VARCHAR(40),
+	agendamentoData DATE,
+	nomeEmpresa VARCHAR(40),
+	cnpj CHAR(14),
+	rua VARCHAR(40),
+	bairro VARCHAR(40),
+	complemento VARCHAR(40),
+	PRIMARY KEY (idagendamento)
+)AUTO_INCREMENT = 10000;
 
 
 
